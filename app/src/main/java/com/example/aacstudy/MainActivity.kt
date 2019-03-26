a44d878d895aa3c566317395ed4f78305a1a6ac4
sobaya-0141 <soba.ha.kenkou@gmail.com>
2019-03-26 15:15:13 +0900

    課題2.2

--- app/src/main/java/com/example/aacstudy/MainActivity.kt
+++ app/src/main/java/com/example/aacstudy/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.aacstudy
 
 import android.os.Bundle
+import android.os.WorkSource
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +14,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val lifecycleObserver = object : LifecycleObserver {
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate(source: LifecycleOwner) {
+                println("ON_CREATE : ${source.lifecycle.currentState.name}")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            fun onStart(source: LifecycleOwner) {
+                println("ON_START : ${source.lifecycle.currentState.name}")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy(source: LifecycleOwner) {
+                println("ON_DESTROY : ${source.lifecycle.currentState.name}")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            fun onPause(source: LifecycleOwner) {
+                println("ON_PAUSE : ${source.lifecycle.currentState.name}")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            fun onResume(source: LifecycleOwner) {
+                println("ON_RESUME : ${source.lifecycle.currentState.name}")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+            fun onStop(source: LifecycleOwner) {
+                println("ON_STOP : ${source.lifecycle.currentState.name}")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+            fun onAny(source: LifecycleOwner, event: Lifecycle.Event) {
+                println("ON_ANY : ${source.lifecycle.currentState.name}")
+            }
+        }
+
+        lifecycle.addObserver(lifecycleObserver)
     }
 }
