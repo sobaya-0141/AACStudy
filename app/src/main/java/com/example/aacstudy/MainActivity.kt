d6b060db5b415cea66c7a62b90ee9d9d3c330435
sobaya-0141 <soba.ha.kenkou@gmail.com>
2019-03-26 15:03:22 +0900

    課題1.1

--- app/src/main/java/com/example/aacstudy/MainActivity.kt
+++ app/src/main/java/com/example/aacstudy/MainActivity.kt
@@ -1,13 +1,44 @@
 package com.example.aacstudy
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("onCreate : ${lifecycle.currentState.name}")
         setContentView(R.layout.activity_main)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        println("onStart : ${lifecycle.currentState.name}")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume : ${lifecycle.currentState.name}")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause : ${lifecycle.currentState.name}")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop : ${lifecycle.currentState.name}")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy : ${lifecycle.currentState.name}")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        println("onSaveInstanceState : ${lifecycle.currentState.name}")
     }
 }
