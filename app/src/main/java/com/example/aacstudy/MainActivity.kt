b71d90a514663dfc44309ba98b543c102437ab4b
sobaya-0141 <soba.ha.kenkou@gmail.com>
2019-03-27 14:33:55 +0900

    課題5.1

--- app/src/main/java/com/example/aacstudy/MainActivity.kt
+++ app/src/main/java/com/example/aacstudy/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.aacstudy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val liveData = MutableLiveData<String>()
+
+        liveData.observe(this, Observer {
+            println(it)
+        })
+
+        button.setOnClickListener {
+            liveData.value = "a"
+            liveData.value = "b"
+        }
     }
 }
