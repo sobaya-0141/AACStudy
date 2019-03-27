ff1a7d815c47cba23d74acc8088c345ccbe48656
sobaya-0141 <soba.ha.kenkou@gmail.com>
2019-03-27 15:12:11 +0900

    課題8.1

--- app/src/main/java/com/example/aacstudy/MainActivity.kt
+++ app/src/main/java/com/example/aacstudy/MainActivity.kt
@@ -3,23 +3,18 @@ package com.example.aacstudy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.lifecycle.Transformations
 
 class MainActivity : AppCompatActivity() {
-
-    private val liveData = CountUpLiveData()
-    private val observer = Observer<Int> { println("observeForever : $it") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        liveData.observe(this, Observer { println(it) })
-        liveData.observeForever(observer)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        liveData.removeObserver(observer)
+        Transformations
+            .map(CountUpLiveData()) { it * it }
+            .observe(this, Observer {
+                println(it)
+            })
     }
 }
